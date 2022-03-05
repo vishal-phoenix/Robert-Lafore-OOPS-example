a7a463a074c026794e2e20c6f5b3c4a65bba7408
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-03-05 17:16:33 +0530

    Initial commit

---
+++ Chapter 2/Ques_3.cpp
@@ -0,0 +1,13 @@
+//exercises arithmetic assignment and decrement
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int gen = 10;
+    cout<<gen<<endl;
+    gen *=2;
+    cout<<gen--<<endl;
+    cout<<gen<<endl;
+    return 0;
+}
