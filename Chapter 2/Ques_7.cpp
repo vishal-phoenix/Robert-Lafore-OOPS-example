7838b98c051b8fb85c589e3f091248c9979e8da7
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-03-05 17:18:45 +0530

    Initial commit

---
+++ Chapter 2/Ques_7.cpp
@@ -0,0 +1,10 @@
+//Temperature Conversion
+#include <iostream>
+using namespace std;
+
+int main(){
+    float celsius;
+    cin>>celsius;
+    cout<<"Fahrenheit : "<<((celsius*9)/5)+32;
+    return 0;
+}
