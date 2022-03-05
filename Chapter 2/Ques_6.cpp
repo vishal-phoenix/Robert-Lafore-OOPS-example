259ef09ec06c408ae08f740bd27b10214a479835
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-03-05 17:18:11 +0530

    Initial commit

---
+++ Chapter 2/Ques_6.cpp
@@ -0,0 +1,13 @@
+//Currency Conversion
+#include <iostream>
+using namespace std;
+
+int main(){
+    double dollar;
+    cin>>dollar;
+    cout<<"British Pound : "<< dollar/1.487 <<endl;
+    cout<<"French Franc : "<< dollar/0.172 <<endl;
+    cout<<"German Deutschemark : "<< dollar/0.584 <<endl;
+    cout<<"Japanese Yen : "<< dollar/0.00955 <<endl;
+    return 0;
+}
