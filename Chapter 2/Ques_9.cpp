a65e4acdbdbaee1ee84bdb6397c2f0bf77906784
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-03-05 17:19:44 +0530

    Initial commit

---
+++ Chapter 2/Ques_9.cpp
@@ -0,0 +1,17 @@
+//Fraction Addition
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d;
+    char dummychar;
+    cout<<"Enter first fraction: ";
+    cin>>a>>dummychar>>b;
+        cout<<"Enter second fraction: ";
+
+    cin>>c>>dummychar>>d;
+    
+    cout<<"Sum: "<< (a*d)+(b*c)<<"/"<<b*d;
+
+    return 0;
+}
