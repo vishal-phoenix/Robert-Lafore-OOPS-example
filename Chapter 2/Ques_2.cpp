1e5d8468cadb74b824f1014dc5f893ba01dc4df8
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-03-05 17:15:58 +0530

    Initial commit

---
+++ Chapter 2/Ques_2.cpp
@@ -0,0 +1,9 @@
+//generate table
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    cout<<"\t1990"<<setw(15)<<"135\n"<<"\t1991"<<setw(15)<<"7290\n"<<"\t1993"<<setw(15)<<"11300\n"<<"\t1994"<<setw(15)<<"16200\n";
+    return 0;
+}
