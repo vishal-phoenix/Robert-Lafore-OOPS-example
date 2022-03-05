94c4e4fae086afd412ffb03313da3d0d50ccedb0
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-03-05 17:17:40 +0530

    initial commit

---
+++ Chapter 2/Ques_5.cpp
@@ -0,0 +1,11 @@
+//lower case or upper case
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+int main(){
+    char insrt;
+    cin>>insrt;
+    cout<<"character is lower case : "<<islower(insrt);
+    return 0;
+}
